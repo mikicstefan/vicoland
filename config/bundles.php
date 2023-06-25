d03b313d2efb43e63d1f0b30bc3496a85e52865c
stefanmikic <mikic.stefan95@gmail.com>
2023-06-25 23:11:10 +0200

    [VICO-3] Indentation fix

--- config/bundles.php
+++ config/bundles.php
@@ -1,15 +1,15 @@
 <?php
 
 return [
-    Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
-    Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
-    Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
-    Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
-    DAMA\DoctrineTestBundle\DAMADoctrineTestBundle::class => ['test' => true],
-    Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
-    Nelmio\ApiDocBundle\NelmioApiDocBundle::class => ['all' => true],
-    Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
-    JMS\SerializerBundle\JMSSerializerBundle::class => ['all' => true],
-    Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
-    Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
+  Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
+  Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
+  Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
+  Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
+  DAMA\DoctrineTestBundle\DAMADoctrineTestBundle::class => ['test' => true],
+  Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
+  Nelmio\ApiDocBundle\NelmioApiDocBundle::class => ['all' => true],
+  Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
+  JMS\SerializerBundle\JMSSerializerBundle::class => ['all' => true],
+  Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
+  Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
 ];
